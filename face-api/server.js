93d83e263d73aa155f4d8a35c33f613ebcd963ae
Sadi Mohammad Mustafa <sadi.amith@live.co.uk>
2019-12-06 03:19:56 -0600

    database transactions added

--- face-api/server.js
+++ face-api/server.js
@@ -25,24 +25,48 @@ app.get('/', (req, res) => {
 })
 
 app.post('/signin', (req, res) => {
-    if(req.body.email === database.users[0].email &&
-        req.body.password === database.users[0].password) {
-            res.json(database.users[0]);
-        } else {
-            res.status(400).json('error logging in');
+    database.select('email', 'hash').from('login')
+    .where('email', '=', req.body.email)
+    .then(data => {
+        const isValid = bcrypt.compareSync(req.body.password, data[0].hash);
+        if (isValid){
+            return database.select('*').from('users')
+            .where('email', '=', req.body.email)
+            .then(user => {
+                res.json(user[0])
+            })
+            .catch(err => res.status(400).json('unable to get user'))
         }
+        else{
+            res.status(400).json('wrong info')
+        }
+    })
+    .catch(err => res.status(400).json('Wrong Credentials'))
 })
 
 app.post('/register', (req, res) => {
     const {name, email, password} = req.body;
-    database('users')
-    .returning('*')
-    .insert({
-        name: name,
-        email: email,
-        joined: new Date()
-    }).then(user => {
-        res.json(user[0]);
+    const hash = bcrypt.hashSync(password);
+    database.transaction(trx => {
+        trx.insert({
+            hash: hash,
+            email: email
+        })
+        .into('login')
+        .returning('email')
+        .then(loginEmail => {
+            return trx('users')
+                .returning('*')
+                .insert({
+                    name: name,
+                    email: loginEmail[0],
+                    joined: new Date()
+                }).then(user => {
+                    res.json(user[0]);
+            })
+        })
+        .then(trx.commit)
+        .catch(trx.rollback)
     })
     .catch(err => res.status(400).json('unable to register'))
 })
