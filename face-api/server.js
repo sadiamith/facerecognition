aa573edd4ff52de2eb5e511dcd2aabff890d24f7
Sadi Mohammad Mustafa <sadi.amith@live.co.uk>
2019-12-19 19:09:52 -0600

    code minified

--- face-api/server.js
+++ face-api/server.js
@@ -7,6 +7,7 @@ const knex = require('knex');
 const register = require('./controllers/register');
 const signin = require('./controllers/signin');
 const profile = require('./controllers/profile');
+const image = require('./controllers/image');
 
 const database = knex({
     client: 'pg',
@@ -22,20 +23,11 @@ const app = express();
 app.use(bodyParser.json());
 app.use(cors());
 
-
-
-app.get('/', (req, res) => {
-    res.send(database.users);
-})
-
+app.get('/', (req, res) => {res.send(database.users)})
 app.post('/signin', (req, res) => {signin.handleSignin(req, res, database, bcrypt)})
-
 app.post('/register', (req, res) => register.handleRegister(req, res, database, bcrypt))
-
 app.get('/profile/:id', (req, res) => {profile.handleProfileGet(req, res, database)})
-
 app.put('/image', (req, res) => {image.handleImage(req, res, database)})
-
 
 app.listen(3001, ()=> {
     console.log('app is running on port 3001');
