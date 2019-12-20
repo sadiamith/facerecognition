1ac494f06a28b20986708b041e45b81acdfc8c55
Sadi Mohammad Mustafa <sadi.amith@live.co.uk>
2019-12-19 22:15:02 -0600

    registration validated

--- face-api/controllers/register.js
+++ face-api/controllers/register.js
@@ -1,5 +1,9 @@
 const handleRegister = (req, res, database, bcrypt) => {
     const {name, email, password} = req.body;
+    if(!email || !name || !password) {
+        return res.status(400).json('Incorrect form submission');
+    }
+    
     const hash = bcrypt.hashSync(password);
     database.transaction(trx => {
         trx.insert({
