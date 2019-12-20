a262d30e30fee1cd5e2fb6d711257372dff745a6
Sadi Mohammad Mustafa <sadi.amith@live.co.uk>
2019-12-19 18:40:10 -0600

    adding controller components

---
+++ face-api/controllers/register.js
@@ -0,0 +1,26 @@
+(req, res) => {
+    const {name, email, password} = req.body;
+    const hash = bcrypt.hashSync(password);
+    database.transaction(trx => {
+        trx.insert({
+            hash: hash,
+            email: email
+        })
+        .into('login')
+        .returning('email')
+        .then(loginEmail => {
+            return trx('users')
+                .returning('*')
+                .insert({
+                    name: name,
+                    email: loginEmail[0],
+                    joined: new Date()
+                }).then(user => {
+                    res.json(user[0]);
+            })
+        })
+        .then(trx.commit)
+        .catch(trx.rollback)
+    })
+    .catch(err => res.status(400).json('unable to register'))
+}
--- face-api/server.js
+++ face-api/server.js
@@ -44,32 +44,7 @@ app.post('/signin', (req, res) => {
     .catch(err => res.status(400).json('Wrong Credentials'))
 })
 
-app.post('/register', (req, res) => {
-    const {name, email, password} = req.body;
-    const hash = bcrypt.hashSync(password);
-    database.transaction(trx => {
-        trx.insert({
-            hash: hash,
-            email: email
-        })
-        .into('login')
-        .returning('email')
-        .then(loginEmail => {
-            return trx('users')
-                .returning('*')
-                .insert({
-                    name: name,
-                    email: loginEmail[0],
-                    joined: new Date()
-                }).then(user => {
-                    res.json(user[0]);
-            })
-        })
-        .then(trx.commit)
-        .catch(trx.rollback)
-    })
-    .catch(err => res.status(400).json('unable to register'))
-})
+app.post('/register', )
 
 app.get('/profile/:id', (req, res) => {
     const { id } = req.params;
