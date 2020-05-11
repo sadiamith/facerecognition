6526db9929ff9553d4ac61952fffed1b30187070
Sadi Mohammad Mustafa <sadi.amith@live.co.uk>
2020-05-10 18:34:00 -0600

    fixed new connection bug in server

--- src/components/Register/Register.js
+++ src/components/Register/Register.js
@@ -22,7 +22,7 @@ class Register extends React.Component {
   }
 
   onSubmitSignIn = () => {
-    fetch('http://localhost:3001/register', {
+    fetch('https://morning-meadow-90005.herokuapp.com/register', {
       method: 'post',
       headers: {'Content-Type': 'application/json'},
       body: JSON.stringify({
