e2712edac24c08c6a964c14741da197ec0cffa83
Sadi Mohammad Mustafa <sadi.amith@live.co.uk>
2020-05-10 18:19:36 -0600

    connecting database

--- face-api/server.js
+++ face-api/server.js
@@ -12,9 +12,9 @@ const image = require('./controllers/image');
 const database = knex({
     client: 'pg',
     connection: {
-      host : '127.0.0.1',
+      host : 'postgresql-concentric-37569',
       user : 'postgres',
-      password : 'parul',
+      password : '',
       database : 'smartbrain'
     }
   });
--- src/components/Signin/Signin.js
+++ src/components/Signin/Signin.js
@@ -18,7 +18,7 @@ class Signin extends React.Component {
   }
 
   onSubmitSignIn = () => {
-    fetch('http://localhost:3001/signin', {
+    fetch('https://morning-meadow-90005.herokuapp.com/signin', {
       method: 'post',
       headers: {'Content-Type': 'application/json'},
       body: JSON.stringify({
